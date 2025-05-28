e4c2212f56f51a5ef5d0b89b3b8b57d0ebec8014
rafialfiandro <87708096+rafialfiandro@users.noreply.github.com>
2025-05-28 14:50:29 +0900

    Added wait for input and tweaked enemy encounter mechanic

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import sys
 import os
 import time
 import random
+import msvcrt
 
 screen_width = 100
+
+# Function to clear screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # Player Definition
 class player:
@@ -41,16 +46,24 @@ enemies = [goblin, minotaur,skeleton,orc,dark_knight]
 # Enemy Encounter Function    
 def enemy_encounter():
     
+    clear_screen()
+    
     enemy = random.choice(enemies)
     action = None
     flee = False
     playerTurn = True
+    enemyDefeat = False
+    playerDefeat = False
     
     print("An enemy encounter!")
     
-    while enemy.hp > 0 or flee != True:
+    while enemyDefeat == False or playerDefeat == False or flee == False:
         
         if enemy.hp <= 0:
+            enemyDefeat = True
+            break
+        elif mainPlayer.hp <= 0:
+            playerDefeat = True
             break
         
         if playerTurn == True:
@@ -65,52 +78,77 @@ def enemy_encounter():
                 action = input("> ")
             
             if action == '1':
+                clear_screen()
                 enemy.hp -= mainPlayer.attack
+                print("Player did", mainPlayer.attack, "damage to", enemy.name)
                 playerTurn = False
                 action = '0'
+                msvcrt.getch()
             elif action == '2':
-                diceRoll = random.randint(75,100)
+                diceRoll = random.randint(75,100) # Remember to reset
                 if diceRoll < 75:
                     print(diceRoll)
                     print('succesfully fleed!')
-                    #flee = True
+                    flee = True
+                    msvcrt.getch()
                     break
                 else:
                     print('you were not able to escape')
-                    action = '0'
+                    action = None
                     playerTurn = False
+                    msvcrt.getch()
             elif action == '3':
+                clear_screen()
                 print("HP:", mainPlayer.hp)
                 print("Attack:", mainPlayer.attack)
                 action = '0'
+                msvcrt.getch()
+                clear_screen()
                 continue
         else:
+            clear_screen()
             print("The enemy attacks you for", enemy.att, "damage")
             mainPlayer.hp -= enemy.att
             playerTurn = True
+            msvcrt.getch()
     
-    print("Enemy defeated!")
-    
-    drop = random.choices(enemy.loot, weights = enemy.chance, k=1)[0]
-    print("It dropped ", drop,"!", sep='')
+    if enemyDefeat == True:
+        print("Enemy defeated!")
+        print("")
+        drop = random.choices(enemy.loot, weights = enemy.chance, k=1)[0]
+        print("It dropped ", drop,"!", sep='')
+        msvcrt.getch()
+        game_start()
+    elif playerDefeat == True:
+        print("You have been defeated")
+        msvcrt.getch()
+        title_screen()
+        
     
         
 def game_start():
+    clear_screen()
     option = None
     print("What do you want to do?")
     print("[1] Enter Dungeon")
     print("[2] Return to Main Menu")
     
-    while option not in ["1", "2"]:
-        option = input()
+    option = input("> ")
+    
+    if option not in ['1','2', None]:
+        option = None
+        game_start()
+    
+    if option == '1':
+        enemy_encounter()
+    elif option == '2':
+        title_screen()
         
-        if option == '1':
-            enemy_encounter()
-        elif option == '2':
-            title_screen()
 
 # Title Screen Function
 def title_screen():
+    
+    clear_screen()
     
     option = None
     
@@ -124,8 +162,11 @@ def title_screen():
     print()
     print("          ==========================")
     
-    while(option not in ['1', '2', '3']):
-        option = input("> ")
+    option = input("> ")
+    
+    if option not in ['1','2','3',None]:
+        option = None
+        title_screen()
     
     if option == "1":
         game_start()
@@ -133,17 +174,13 @@ def title_screen():
     elif option == "2":
         #credits()
         print("made by me")
+        print("press any key to return to main menu")
+        msvcrt.getch()
+        title_screen()
     elif option == "3":
-        print('Thank you for playing!')
         sys.exit()
-    else:
-        print("input not recognized")
      
 # Call in-game Objects   
 mainPlayer = player()
 
 title_screen()
-
-
-        
-        
